243ff5aad959f1e234f962ff63e7c6cf954d550a
moonjinhwan <moonjinh123@naver.com>
2020-05-14 17:58:47 +0900

    링크 설정

--- src/kr/co/myboard/controller/HomeController.java
+++ src/kr/co/myboard/controller/HomeController.java
@@ -11,7 +11,7 @@ public class HomeController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home() {
 		System.out.println("home");
-		return "index";
+		return "main";
 	}
 	
 }
